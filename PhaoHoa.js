11789327e5d457680d498d328d530f56f730e298
zenvn12 <anvuminh0@gmail.com>
2025-01-10 20:53:23 +0700

    Update PhaoHoa.js

--- PhaoHoa.js
+++ PhaoHoa.js
@@ -401,6 +401,9 @@ Object.keys(appNodes).forEach(key => {
 if (!fullscreenEnabled()) {
 	appNodes.fullscreenFormOption.classList.add('remove');
 }
+appNodes.closeMenuBtn.addEventListener('click', () => {
+    toggleMenu(false);
+});
 
 // First render is called in init()
 function renderApp(state) {
